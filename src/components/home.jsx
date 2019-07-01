efc7782d70f235b9db78064b49753f1bf4ad6719
Jacob Allen <boat.mon@gmail.com>
2019-07-01 17:20:46 -0400

    added a mapbox map

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import Map from './Map';
 
-export default function Map() {
+
+export default function Home() {
     return <div>home</div>;
   }
--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,5 +1,28 @@
-import React form "react";
+import React, {useState} form "react";
+import './Map.css';
+import ReactMapGL from "react-map-gl"
 
-export default function Map() {
-    return <div>Map</div>;
-  }
+export  default function App() {
+const [viewport, setViewport] = useState({
+  latitude:25.774266,
+  longitude:-80.193659,
+  width: '100ve',
+  height: '100vh',
+  zoom: 10
+});
+
+  return (
+    <div>
+      <ReactMapGL {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        mapStyle="mapbox://styles/loghorizon/cjxdob3o109pf1cqmir78azk5"
+        onViewportChange={viewport => {
+          setViewport(viewport);
+        }}
+        >
+        Markers here
+
+      </ReactMapGL>
+    </div>
+  );
+}
