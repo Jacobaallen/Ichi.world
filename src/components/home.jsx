1876a8d254de323d3ff60a4de491969c188e561c
Jacob Allen <boat.mon@gmail.com>
2019-07-25 17:26:44 -0400

    working on making a draw for the home page

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Map from './Map';
+import Forex from './Forex';
+import Drawer from 'react-drag-drawer';
+import '../styles/Home.css';
 
 
-export default function Home() {
-    return <Map/>
+
+class Home extends Component {
+  render () {
+    return(
+      <div className="master">
+        <div className="map">
+          <Map/>
+        </div>
+
+        <div className="moneyex">
+
+        </div>
+      </div>
+    )
   }
+}
+export default Home;
