50222a11c66bead9d6f6cddf1e9dca71eb2a718f
Jacob Allen <boat.mon@gmail.com>
2019-08-20 17:21:43 -0400

    working on fixing the component

--- src/components/Draw1.jsx
+++ src/components/Draw1.jsx
@@ -4,26 +4,35 @@ import Forex from './Forex';
 import '../styles/Draw1.css';
 
 
-toggle = () => {
-  let { toggle } = this.state
 
-  this.setState({ toggle: !toggle })
-}
 
 class Draw1 extends React.Component {
-render() {
-  const { open } = this.state
+  state = {
+  toggle: ''
+}
+  return(
+    toggle = () => {
+    let toggle = this.state
 
-  return (
-    <Drawer
-      open={open}
-      onRequestClose={this.toggle}
-    >
-      <div className="money">
-        <Forex/>
-      </div>
-    </Drawer>
-  );
-}};
+    this.state({toggle: !toggle}
+  }))
+//    toggle (){
+//     let toggle = this.state
+//
+//     this.state({toggle: !toggle});
+//   }
+//   render() {
+//     const { open } = this.state
+// //   return (
+// //     <Drawer
+// //       open={open}
+// //       onRequestClose={this.toggle}
+// //     >
+// //       <div className="money">
+// //         <Forex/>
+// //       </div>
+// //     </Drawer>
+// //   );}
+};
 
 export default Draw1;
