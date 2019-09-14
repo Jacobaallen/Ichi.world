303ff37207d394ee8c263a4e13c2f22d6905c35f
Jacob Allen <boat.mon@gmail.com>
2019-09-14 15:43:44 -0400

    fix title

--- src/components/Museums.jsx
+++ src/components/Museums.jsx
@@ -6,8 +6,8 @@ class Museums extends Component {
   render () {
     return(
       <div className="master">
-        <div className="top">
-
+        <div className="top-title-1">
+          <h1>Museums</h1>
         </div>
         <div className="map" id="map">
           <Map/>
