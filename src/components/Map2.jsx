3db07a6a5a032c104462c8d217cf2afbe2dae4b0
Jacob Allen <boat.mon@gmail.com>
2019-10-04 17:07:48 -0400

    update map2

--- src/components/Map2.jsx
+++ src/components/Map2.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import ReactMapGL, { Marker } from "react-map-gl";
+import React, { useState, useEffect } from 'react';
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
 import * as attractionData from "./Attraction.json";
 import "../styles/Map2.css";
 
@@ -12,6 +12,19 @@ const [viewport, setViewport] = useState({
   height: '100vh',
   zoom: 10
 });
+const [selectedMuseum, setSelectetMuseum] = useState(null);
+
+//this is to use the button esc on the key board.
+  useEffect(() => {
+    const listener = e => {
+      if (e.key === "Escape") {
+        setSelectetMuseum(null);
+      }
+    };
+    window.addEventListener("keydown", listener);
+
+
+  }, []);
 
   return (
     <div>
@@ -22,20 +35,39 @@ const [viewport, setViewport] = useState({
           setViewport(viewport);
         }}
         >
-        {attractionData.features.map((attraction)=>(
+        {attractionData.features.map(museum => (
           <Marker
-            key={attraction.properties.ATTRACTION_ID}
-            latitude={attraction.geometry.coordinates[1]}
-            longitude={attraction.geometry.coordinates[0]}
+            key={museum.properties.MUSEUM_ID}
+            latitude={museum.geometry.coordinates[1]}
+            longitude={museum.geometry.coordinates[0]}
             >
 
-            <button className="marker-btn">
+            <button className="marker-btn" onClick={(e) => {
+                e.preventDefault();
+                setSelectetMuseum(museum)
+              }}
+            >
               <img src="/images/Museum.png" alt="Museum Icon" />
             </button>
           </Marker>
         ))}
 
-
+        {selectedMuseum ? (
+          //This is all happening in a Popup.
+          <Popup
+             latitude={selectedMuseum.geometry.coordinates[1]}
+             longitude={selectedMuseum.geometry.coordinates[0]}
+             onClose={() => {
+               setSelectetMuseum(null);
+             }}
+             >
+            <div>
+              <h2>{selectedMuseum.properties.NAME}</h2>
+              <p>{selectedMuseum.properties.DESCRIPTIO}</p>
+              <p>{selectedMuseum.properties.TIME}</p>
+            </div>
+          </Popup>
+        ) : null}
       </ReactMapGL>
     </div>
 
