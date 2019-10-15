8a91f6ceac0224010b4d72204869bb7683abda6d
Jacob Allen <boat.mon@gmail.com>
2019-10-15 16:08:45 -0400

    adding context

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -9,9 +9,23 @@ class Home extends Component {
   render () {
     return(
       <div className="master">
-        <div className="hero-image">
+        <div className="logo-image">
 
         </div>
+        <div className="about-master">
+          <div className="about-header">
+            <h2>About</h2>
+          </div>
+            <div className="about-text">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Non blandit massa enim nec dui nunc. Aliquam sem et tortor consequat id. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque.
+                Odio morbi quis commodo odio aenean sed. Morbi tincidunt augue interdum velit euismod in. Sit amet mattis vulputate enim nulla aliquet porttitor lacus luctus.
+                Libero justo laoreet sit amet cursus. Elit pellentesque habitant morbi tristique senectus et netus et malesuada. Laoreet sit amet cursus sit amet.
+                Faucibus in ornare quam viverra orci sagittis eu volutpat odio. Egestas quis ipsum suspendisse ultrices.
+              </p>
+            </div>
+          </div>
+
 
 
 
