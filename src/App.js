4a0398007ee323ba2333139f52bdd2ee5e27b73c
Jacob Allen <boat.mon@gmail.com>
2019-10-31 12:16:23 -0400

    updated link for events

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Footer from './components/Footer';
 import Museums from './components/Museums';
 import Attraction from './components/Attract';
 import Drop from './components/Drop';
+import Events from './components/Events';
 
 class App extends React.Component{
   render () {
@@ -18,6 +19,7 @@ class App extends React.Component{
       <Route exact path="/forex" component={Forex} />
       <Route exact path="/museums" component={Museums} />
       <Route exact path="/attract" component={Attraction} />
+      <Route exact path="/events" component={Events} />
     </Router>
   )}
 }
