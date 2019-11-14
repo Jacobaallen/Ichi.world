62d25980da3ff9b590cf2a6359f9acd17f145a1a
Jacob Allen <boat.mon@gmail.com>
2019-11-14 15:01:22 -0500

    added the logo to the page

--- src/components/Museums.jsx
+++ src/components/Museums.jsx
@@ -7,6 +7,9 @@ class Museums extends Component {
     return(
       <div className="master">
         <div className="top-title-1">
+          <div className="logo-head">
+            <a href="/"><img src="/images/name-logo.png" alt="logo" height="30"/></a>
+          </div>
           <h1>Museums</h1>
         </div>
         <div className="map" id="map">
