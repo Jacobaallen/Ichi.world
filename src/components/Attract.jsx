56d8166b46f7aa890062314b78f4447f9e66d959
Jacob Allen <boat.mon@gmail.com>
2019-11-14 15:00:10 -0500

    updates

--- src/components/Attract.jsx
+++ src/components/Attract.jsx
@@ -7,6 +7,9 @@ class Attraction extends Component {
     return(
       <div className="master">
         <div className="top-title-2">
+          <div className="logo-head">
+            <a href="/"><img src="/images/name-logo.png" alt="logo" height="30"/></a>
+          </div>
           <h1>Attractions</h1>
         </div>
         <div className="map2" id="map2">
