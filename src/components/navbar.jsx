629dbff8ab746c3621811eee009b973ab3152ebf
Jacob Allen <boat.mon@gmail.com>
2019-11-14 15:56:49 -0500

    update

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -12,6 +12,7 @@ class Navbar extends Component{
             <li><a href="/Attract">Attraction Map</a></li>
             <li><a href="/Museums">Museum Map</a></li>
             <li><a href="/Events">Events</a></li>
+            <li><a href="/LogBook">Log Book</a></li>
         </div>
       )
     }
