dea8c1640e3b2ecd313817699a666d21518366c6
Jacob Allen <boat.mon@gmail.com>
2019-11-19 18:47:21 -0500

    added the corner logo to it

--- src/components/Drop.jsx
+++ src/components/Drop.jsx
@@ -6,6 +6,9 @@ class Blog extends Component {
   render () {
     return(
       <div className="dropdown-master">
+        <div className="logo">
+          <a href="/"><img src="/images/giphy.gif" alt="logo" width="47" height="55"/></a>
+        </div>
         <div className="dropdown">
           <span>Foreign Currency Rates</span>
           <div className="dropdown-content">
