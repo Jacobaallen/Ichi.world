8147a4fe18db6d608823ab21aa105928c02ab13f
Jacob Allen <boat.mon@gmail.com>
2019-11-19 18:48:34 -0500

    move logo from nav to drop

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,7 +6,6 @@ class Navbar extends Component{
   render () {
     return (
         <div className="nav">
-            <li><a href="/"><img src="/images/giphy.gif" alt="logo" width="47" height="55"/></a></li>
             <li><a href="/">Home</a></li>
             <li><a href="/#about">About</a></li>
             <li><a href="/Attract">Attraction Map</a></li>
